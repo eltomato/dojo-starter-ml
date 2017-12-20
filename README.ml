5ea3f026eb89ce1ff5d47891e3244c7f3a3a9cba
Tommaso Previero <tommaso.previero@lastminute.com>
2017-12-21 00:00:21 +0100

    Remove an unused dependency. Add README.md

---
+++ README.ml
@@ -0,0 +1,12 @@
+dojo-starter-ml
+===============
+
+A simple starter to use for simple coding dojos or simply to do some
+experiments with Standard ML.
+To clone the project properly please use
+```bash
+git clone --recurse-submodules https://github.com/eltomato/dojo-starter-ml.git
+```
+because the project is using as dependencies (git submodules):
+[OMLoader](https://github.com/eltomato/OMLoader)
+[sml-testing](https://github.com/kvalle/sml-testing)
